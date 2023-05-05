e22f58c9fd3af7bc90c1ff89c58ca0c8562a6196
Callum Kloos <callumkloos@gmail.com>
2023-05-05 15:12:48 +0100

    simple dates

--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -1,66 +1,66 @@
 import moment from "moment";
 import { TaskType } from "../../types";
 import {
-	StyledTask,
-	StyledTaskBody,
-	StyledTaskCheckbox,
-	StyledTaskTitle,
+    StyledTask,
+    StyledTaskBody,
+    StyledTaskCheckbox,
+    StyledTaskTitle,
 } from "../styles";
 import { PrimaryButton, WarningButton } from "../styles/Button";
 
 interface TaskProps {
-	task: TaskType;
-	deleteTask: (id: number) => void;
-	archiveTask: (id: number) => void;
-	toggleTaskCompleted: (task: TaskType) => void;
+    task: TaskType;
+    deleteTask: (id: number) => void;
+    archiveTask: (id: number) => void;
+    toggleTaskCompleted: (task: TaskType) => void;
 }
 
 //
 // COMPONENT
 //
 export const Task = ({
-	task,
-	deleteTask,
-	archiveTask,
-	toggleTaskCompleted,
+    task,
+    deleteTask,
+    archiveTask,
+    toggleTaskCompleted,
 }: TaskProps) => {
-	const handleDelete = () => {
-		if (typeof task.id === "number") {
-			deleteTask(task.id);
-		}
-	};
+    const handleDelete = () => {
+        if (typeof task.id === "number") {
+            deleteTask(task.id);
+        }
+    };
 
-	const handleArchive = () => {
-		if (typeof task.id === "number") {
-			archiveTask(task.id);
-		}
-	};
+    const handleArchive = () => {
+        if (typeof task.id === "number") {
+            archiveTask(task.id);
+        }
+    };
 
-	const handleEdit = () => {
-		console.log("Edit task");
-	};
+    const handleEdit = () => {
+        console.log("Edit task");
+    };
 
-	const handleCompletedToggled = () => {
-		toggleTaskCompleted(task);
-	};
+    const handleCompletedToggled = () => {
+        toggleTaskCompleted(task);
+    };
 
-	return (
-		<StyledTask>
-			<StyledTaskTitle>{task.title}</StyledTaskTitle>
-			<StyledTaskBody>
-				<div>{task.body}</div>
-				<div>
-					DUE: {task.dueDate && moment(task.dueDate).format("ddd, MMM YYYY")}
-				</div>
-			</StyledTaskBody>
-			<StyledTaskCheckbox
-				type="checkbox"
-				onChange={handleCompletedToggled}
-				checked={task.completed}
-			/>
-			<WarningButton onClick={handleDelete}>Delete</WarningButton>
-			<PrimaryButton onClick={handleEdit}>Edit</PrimaryButton>
-			<PrimaryButton onClick={handleArchive}>Archive</PrimaryButton>
-		</StyledTask>
-	);
+    return (
+        <StyledTask>
+            <StyledTaskTitle>{task.title}</StyledTaskTitle>
+            <StyledTaskBody>
+                <div>{task.body}</div>
+                <div>
+                    DUE: {task.dueDate && moment(task.dueDate).format("ddd, MMM YYYY")}
+                </div>
+            </StyledTaskBody>
+            <StyledTaskCheckbox
+                type="checkbox"
+                onChange={handleCompletedToggled}
+                checked={task.completed}
+            />
+            <WarningButton onClick={handleDelete}>Delete</WarningButton>
+            <PrimaryButton onClick={handleEdit}>Edit</PrimaryButton>
+            <PrimaryButton onClick={handleArchive}>Archive</PrimaryButton>
+        </StyledTask>
+    );
 };
--- src/utils/tasksApi.ts
+++ src/utils/tasksApi.ts
@@ -2,82 +2,82 @@ import { TaskType, ClientTaskType } from "../types";
 import { API_URL } from "../../config";
 
 export async function fetchTasksFromServer(): Promise<TaskType[]> {
-	try {
-		const response = await fetch(`${API_URL}/tasks`);
-		return await response.json();
-	} catch (error) {
-		console.error(error);
-		return [];
-	}
+  try {
+    const response = await fetch(`${API_URL}/tasks`);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
 export async function postTaskToServer(task: ClientTaskType): Promise<Response> {
-	const options = {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(task),
-	};
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  };
 
-	const response = await fetch(`${API_URL}/tasks`, options);
-	if (!response.ok) {
-		throw new Error(`failed to post task!`);
-	}
+  const response = await fetch(`${API_URL}/tasks`, options);
+  if (!response.ok) {
+    throw new Error(`failed to post task!`);
+  }
 
-	return response;
+  return response;
 }
 
 export async function markTaskAsDeletedOnServer(id: number): Promise<Response> {
-	const options = {
-		method: "PATCH",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ deleted: true }),
-	};
+  const options = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ deleted: true }),
+  };
 
-	const response = await fetch(`${API_URL}/tasks/${id}`, options);
-	if (!response.ok) {
-		throw new Error(`failed to mark task as deleted!`);
-	}
+  const response = await fetch(`${API_URL}/tasks/${id}`, options);
+  if (!response.ok) {
+    throw new Error(`failed to mark task as deleted!`);
+  }
 
-	return response;
+  return response;
 }
 
 export async function markTaskAsArchivedOnServer(id: number): Promise<Response> {
-	const options = {
-		method: "PATCH",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ archived: true }),
-	};
+  const options = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ archived: true }),
+  };
 
-	const response = await fetch(`${API_URL}/tasks/${id}`, options);
-	if (!response.ok) {
-		throw new Error(`failed to mark task as archived!`);
-	}
+  const response = await fetch(`${API_URL}/tasks/${id}`, options);
+  if (!response.ok) {
+    throw new Error(`failed to mark task as archived!`);
+  }
 
-	return response;
+  return response;
 }
 
 // Probably change this to a toggle completed function
 export async function toggleTaskCompletedOnServer(task: TaskType): Promise<Response> {
-	const options = {
-		method: "PATCH",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ completed: !task.completed }),
-	};
+  const options = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ completed: !task.completed }),
+  };
 
-	const response = await fetch(`${API_URL}/tasks/${task.id}`, options);
-	if (!response.ok) {
-		throw new Error(`failed to toggle task completed!`);
-	}
+  const response = await fetch(`${API_URL}/tasks/${task.id}`, options);
+  if (!response.ok) {
+    throw new Error(`failed to toggle task completed!`);
+  }
 
-	return response;
+  return response;
 }
 
 // export async function markTaskAsCompletedOnServer(id: number): Promise<Response> {
